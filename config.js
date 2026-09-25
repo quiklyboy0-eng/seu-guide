5779ef20789fed199ca4c75ac8f704ff29cc638e
quiklyboy0-eng <quiklyboy0@gmail.com>
2026-09-25 17:58:04 +0200

    Connect the SEU Guide site to the deployed Cloudflare Worker

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ window.SEU_CONFIG={
   requiredRoleIds:["1525668434074144890"],
   editorRoleIds:["1532108760024354947","1525669484084334714","1525668434120020134"],
   redirectUri:"https://quiklyboy0-eng.github.io/seu-guide/callback.html",
-  authApiUrl:"",
-  publishApiUrl:"",
+  authApiUrl:"https://seu-auth.quiklyboy0.workers.dev",
+  publishApiUrl:"https://seu-auth.quiklyboy0.workers.dev",
   githubRepo:"quiklyboy0-eng/seu-guide",
   githubPath:"content.json"
 };
